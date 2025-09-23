69b14ea990587a67ffb11047133c0e9b63520fc9
GET Academy <terje@getacademy.no>
2025-09-23 19:30:14 +0200

    Update model.js

--- js/model.js
+++ js/model.js
@@ -3,7 +3,9 @@ const colCount = 7;
 const rowCount = 6;
 let turn = 1; // 1 = player 1
               // 2 = player 2 
+              // 0 = game over, ingen har vunnet
               // -1 = game over, player 1 har vunnet
+              // -2 = game over, player 2 har vunnet
 let board = [
     0, 0, 0, 0, 0, 0, 0, // øverste rad
     0, 0, 0, 0, 0, 0, 0,
